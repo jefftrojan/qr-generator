e5874b8e8f8fbb9dc02f342a91dd0fbdc676eacc
Jeff Trojan <j.dauda@alustudent.com>
2023-01-31 01:19:27 +0200

    code generator func

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import QRCode from 'qrcode'
 
 function App() {
+  const [url, setUrl] = useState('')
+  const [qrcode, setQrcode] = useState('')
+
+  const Generatecode = () => {
+    QRCode.toDataURL(url, (err, url) => {
+      if (err) return console.error(err, )
+
+      console.log(url)
+      setQrcode(url)
+    })
+  }
   
 
   return (
@@ -10,10 +22,13 @@ function App() {
       <h1> QR Code Generator</h1>
       <input
       type="text"
+      value={url}
+      onChange={(evt)=> setUrl(evt.target.value)}
       placeholder="E.g https://website.com" />
 
-      <button>
-        Generate QR Code
+      <button onClick={Generatecode}>
+        Get QR Code!
+        <img src={qrcode} />
       </button>
       
     </div>
